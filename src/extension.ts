8e6e4e1e422eeb6ddccfcf36a16e75618071e127
Philipp A. <flying-sheep@web.de>
2026-04-05 15:06:21 +0200

    update lints

--- src/extension.ts
+++ src/extension.ts
@@ -20,7 +20,7 @@ export async function activate(context: ExtensionContext): Promise<Api> {
 		HatchExecutableTracker.create(log),
 		getEnvExtApi(),
 	])
-	await setWorkspacePersistentState(context) // resolves instantly
+	setWorkspacePersistentState(context)
 	const envManager = new HatchEnvManager(api, exe, log)
 	const pkgManager = new HatchPackageManager(api, exe, log)
 	context.subscriptions.push(
