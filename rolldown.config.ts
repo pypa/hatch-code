f3af7f66e4be0dd3648a67bcac4d845f86180345
Phil Schaf <flying-sheep@web.de>
2026-05-10 15:48:39 +0200

    fix transformer regex

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 				},
 			}),
 			// Only run this transform if the file contains a decorator.
-			{ transform: { code: '@' } },
+			{ transform: { code: /^\w*@/ } },
 		),
 		// https://github.com/npm/node-which/issues/174
 		esmExternalRequirePlugin({ external: [/^node:/, 'path'] }),
