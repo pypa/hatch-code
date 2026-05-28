e66ea034d63f310fd5677442dd9ba4613f3ed781
Phil Schaf <flying-sheep@web.de>
2026-05-28 12:41:31 -0400

    error instead

--- src/commands.ts
+++ src/commands.ts
@@ -11,13 +11,19 @@ import type { HatchEnvManager } from './hatch-env-manager.js'
  */
 export async function getEnvInterpreter(
 	envManager: HatchEnvManager,
-	env: string | undefined = 'default',
+	envName: string | undefined = 'default',
 	workspaceDir?: string | undefined,
 ): Promise<string> {
 	const workspaceUri = workspaceDir
 		? Uri.file(workspaceDir)
 		: workspace.workspaceFolders?.[0]?.uri
+	if (!workspaceUri) throw new Error('No workspace open')
 	await envManager.refresh(workspaceUri)
-	const envs = await envManager.getEnvironments(workspaceUri ?? 'all')
-	return envs.find((e) => e.name === env)?.execInfo.run.executable ?? 'python'
+	const envs = await envManager.getEnvironments(workspaceUri)
+	const env = envs.find((e) => e.name === envName)
+	if (!env)
+		throw new Error(
+			`Environment “${envName}” not found in workspace “${workspaceUri.fsPath}”`,
+		)
+	return env.execInfo.run.executable
 }
